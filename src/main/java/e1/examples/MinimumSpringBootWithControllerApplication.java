f35aa43930d2acb65de9797438082abb36453dfb
Eric <eric.writer@copart.com>
2016-12-28 12:57:09 -0800

    First Commit

---
+++ src/main/java/e1/examples/MinimumSpringBootWithControllerApplication.java
@@ -0,0 +1,12 @@
+package e1.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimumSpringBootWithControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimumSpringBootWithControllerApplication.class, args);
+	}
+}
---
+++ src/test/java/e1/examples/MinimumSpringBootWithControllerApplicationTests.java
@@ -0,0 +1,16 @@
+package e1.examples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinimumSpringBootWithControllerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
